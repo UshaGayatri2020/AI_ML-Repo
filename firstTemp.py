6ad78d575222e5548a820f9cc9195579aaaecf79
UshaGayatri2020 <ushagayatri@gmail.com>
2026-02-26 18:02:14 -0600

    I am adding a new file

---
+++ firstTemp.py
@@ -0,0 +1,31 @@
+
+
+
+class FirstTemp:
+   pass
+
+
+a1=90  #global 
+
+
+
+
+class Car:
+   wheels=4  #class variable
+   def move(self):
+       print("I am Moving ",self)
+
+BWM=Car()
+Volkwagen=Car() 
+i20 = Car()
+
+print(BWM.wheels)
+print(Volkwagen.wheels)
+print(Volkwagen.move())
+print(BMW.move())
+
+
+
+
+
+   
